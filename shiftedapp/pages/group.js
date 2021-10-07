43d57722a32e4ad3599497622cb4304a87fcd1ac
TylerD2K <tdo45@my.bcit.ca>
2021-10-07 00:00:37 -0700

    created Group Creation Page
    styling etc...

--- shiftedapp/pages/group.js
+++ shiftedapp/pages/group.js
@@ -2,19 +2,30 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import ButtonsCont from '../comps/HeaderButtons'
-import MyCalendar from '../comps/CalendarBon'
-import MyLegend from '../comps/CalendarLegendBon'
+import GCinput from '../comps/CRinputMat'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <div className={styles.navbar}>
-        <ButtonsCont className={styles.buttons} color={"#FFFFFF"} bg={"#7BE4AB"} txt={"28px"} p={"Calendar"} routeTo="/" ></ButtonsCont>
+        <ButtonsCont className={styles.buttons} color={"#FFFFFF"} bg={"#C4C4C4"} txt={"28px"} p={"Calendar"} routeTo="/" ></ButtonsCont>
         <ButtonsCont className={styles.buttons} color={"#FFFFFF"} bg={"#C4C4C4"} txt={"18px"} p={"Create Schedule"} routeTo="schedule"></ButtonsCont>
-        <ButtonsCont className={styles.buttons} color={"#FFFFFF"} bg={"#C4C4C4"} txt={"18px"} p={"Group Creation"} routeTo="group" ></ButtonsCont>
+        <ButtonsCont className={styles.buttons} color={"#FFFFFF"} bg={"#7BE4AB"} txt={"18px"} p={"Group Creation"} routeTo="group" ></ButtonsCont>
       </div>
       <div className={styles.main}>
-    
+        <div className={styles.list}>
+          <div className={styles.listtitle}>Lead Group Member</div>
+          <GCinput defaultValue="Name" ws="400px"></GCinput>
+          <GCinput defaultValue="Email Address" ws="400px"></GCinput>
+          <div className={styles.listtitle}>Group Member #1</div>
+          <GCinput defaultValue="Name" ws="400px"></GCinput>
+          <GCinput defaultValue="Email Address" ws="400px"></GCinput>
+          <div className={styles.listtitle}>Group Member #2</div>
+          <GCinput defaultValue="Name" ws="400px"></GCinput>
+          <GCinput defaultValue="Email Address" ws="400px"></GCinput>
+
+          <ButtonsCont className={styles.buttons} color={"#FFFFFF"} bg={"#7BE4AB"} txt={"28px"} p={"Create"} routeTo="/" ></ButtonsCont>
+        </div>
       </div>
     </div>
   )
